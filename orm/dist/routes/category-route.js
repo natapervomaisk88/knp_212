ec62b5f44afc521031230d315ed7230a2cce3363
Natalya Babenko <ifelseiforelif@gmail.com>
2024-10-28 15:27:15 +0200

    deleted cert

--- orm/dist/routes/category-route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.categoryRouter = void 0;
-const express_1 = require("express");
-const category_controller_1 = require("../controllers/category-controller");
-exports.categoryRouter = (0, express_1.Router)();
-exports.categoryRouter.route("/").post(category_controller_1.CategoryController.create);
